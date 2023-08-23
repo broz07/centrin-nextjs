96c25c712db630a651be391470129722172ee355
Daniel Brož <daniel.broz@o2.cz>
2023-08-23 21:10:38 +0200

    Add title for pages and skeleton for loading pages

--- app/src/app/defects/history/page.tsx
+++ app/src/app/defects/history/page.tsx
@@ -2,7 +2,13 @@ import DefectHistoryPage from '@centrin/components/defects/history/DefectHistory
 import { DefectContextProvider } from '@centrin/contexts/DefectPage/DefectContext';
 import { UserContextProvider } from '@centrin/contexts/UserContext';
 import { getUser, isLogged } from '@centrin/utils/server/auth';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+	title: 'Historie závad - Centrin',
+	description: 'Stránka s historií závad v systému DOD Centrin',
+};
 
 const DefectHistory = async () => {
 	const logged = await isLogged();
--- app/src/app/defects/page.tsx
+++ app/src/app/defects/page.tsx
@@ -2,7 +2,13 @@ import DefectsPage from '@centrin/components/defects/DefectsPage';
 import { DefectContextProvider } from '@centrin/contexts/DefectPage/DefectContext';
 import { UserContextProvider } from '@centrin/contexts/UserContext';
 import { getUser, isLogged } from '@centrin/utils/server/auth';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+	title: 'Aktivní závady - Centrin',
+	description: 'Stránka s aktivními závadami v systému DOD Centrin',
+};
 
 const Defects = async () => {
 	const logged = await isLogged();
